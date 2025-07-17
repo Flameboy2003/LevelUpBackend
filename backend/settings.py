6622d4d0bc5376d6ea2b9b71b25cd1b8d99b68e7
Akilan M <akilanm416@gmail.com>
2025-07-17 09:55:59 +0530

    cross orgin fixes

--- backend/settings.py
+++ backend/settings.py
@@ -81,6 +81,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
